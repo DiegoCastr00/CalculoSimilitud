c15fad9b08fc21e9490963502a5ec80cda49830a
DiegoCastr00 <diego.castro.elvira@gmail.com>
2025-03-09 00:36:11 -0600

    new compressor

--- huggingface/compressor.py
+++ huggingface/compressor.py
@@ -1,17 +1,34 @@
 import tarfile
 import os
 
-input_folder = "imagenes/resizeSD"
-metadata_file = "metadata.csv"
-output_tar_file = "wikiart_dataset.tar"
+# Rutas de entrada
+input_folder = "imagenes/resize768"  # Carpeta con las imágenes
+metadata_file_1 = "data.csv"            # Primer archivo CSV con los metadatos
+# metadata_file_2 = "metadata.csv"     # Segundo archivo CSV adicional
+output_tar_file = "dataset.tar.gz"      # Nombre del archivo comprimido
 
-with tarfile.open(output_tar_file, "w") as tar:
+# Crear el archivo .tar.gz
+with tarfile.open(output_tar_file, "w:gz") as tar:
+    # Añadir las imágenes
     for root, dirs, files in os.walk(input_folder):
         for file in files:
             file_path = os.path.join(root, file)
+            # Calcular la ruta relativa dentro de la carpeta "images/"
             arcname = os.path.relpath(file_path, input_folder)
-            tar.add(file_path, arcname=f"images/{arcname}")
+            arcname = os.path.join("images", arcname.replace("\\", "/"))  # Asegurar diagonales correctas
+            tar.add(file_path, arcname=arcname)
     
-    tar.add(metadata_file, arcname="metadata.csv")
+    # Añadir el primer archivo CSV
+    if os.path.exists(metadata_file_1):
+        tar.add(metadata_file_1, arcname="data.csv")
+    else:
+        
+        raise FileNotFoundError(f"El archivo {metadata_file_1} no fue encontrado.")
+    
+    # # Añadir el segundo archivo CSV
+    # if os.path.exists(metadata_file_2):
+    #     tar.add(metadata_file_2, arcname="additional_data.csv")
+    # else:
+    #     print(f"Advertencia: El archivo {metadata_file_2} no fue encontrado. No se agregará al archivo comprimido.")
 
 print(f"Archivo {output_tar_file} creado exitosamente.")
--- huggingface/huggingFace.py
+++ huggingface/huggingFace.py
@@ -2,15 +2,16 @@ from huggingface_hub import HfApi
 
 api = HfApi()
 token = ""
-repo_id = "Dant33/Wikiart"
+repo_id = "Dant33/WikiArt-81K-BLIP_2-768x768"
 repo_type = "dataset" 
 
 api.upload_file(
-    path_or_fileobj="wikiart_dataset.tar",
-    path_in_repo="wikiart_dataset.tar",
+    path_or_fileobj="dataset.tar.gz",
+    path_in_repo="dataset.tar.gz",
     repo_id=repo_id,
     token=token,
     repo_type=repo_type,
 )
 
 print("Archivo subido exitosamente a Hugging Face.")
+
